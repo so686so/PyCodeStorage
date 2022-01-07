679ea6ece24a6d79365e4901ce82fe2522ecb1d4
SoByungJun <sbj@gnetsystem.com>
2022-01-07 14:41:08 +0900

    main fix

--- main.py
+++ main.py
@@ -9,29 +9,15 @@ from Core.CodeManage    import CodeManage
 from CoreDefine         import *
 
 
-PATH = r''
-
-CodeListDict = {}
-
-
-# json = JsonManage()
-# json.openJsonFile(JSON_PATH)
-# CodeListDict = json.getJsonDict()
-
-# ChoiceProgram = 'AttributeProgram'
-
-# PATH = CodeListDict[ChoiceProgram]['Path']
-# FILE = CodeListDict[ChoiceProgram]['RunFileName']
-
-# print(f'START : {PATH} -> {FILE}')
-# os.chdir(PATH)
-# a = subprocess.run(f'python {FILE}')
-
 if __name__ == "__main__":
     CM = CodeManage()
     
     Num = CM.getNumByAlias('AttributeProgram')
+    # Num = CM.getNumByAlias('PyToggle')
+    
     print(f'> SelectNum : {Num}')
+    print(f'> Move Dir  : {CM.getCodePathList()[Num]}')
+    print(f'> Run File  : {CM.getCodeFileNameList()[Num]}')
     
     CM.moveTargetDir(CM.getCodePathList()[Num])
     CM.runCode(CM.getCodeFileNameList()[Num])
